ae027cab83a8a35bec1be9802eb06a7b74a705cb
Adam Mohammed <adam@fixergrid.net>
2023-04-17 19:46:53 -0400

    Add early-init to emacs configs

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,10 @@
+;;; early-init.el --- Basic Bootstrapping
+;;; Commentary:
+;;
+;; This just turns off `package.el' in favor of `straight.el'.
+
+;;; Code:
+(setq package-enable-at-startup nil)
+
+(provide 'early-init)
+;;; early-init.el ends here
