8c4c9ff289f3e561bea881fb2db6eb5a877f2a51
Adam Mohammed <adam@fixergrid.net>
2023-05-20 21:34:47 -0400

    add direnv hooks

--- .bashrc.d/01-environment.sh
+++ .bashrc.d/01-environment.sh
@@ -14,3 +14,9 @@ PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 if [[ -d "$HOME/.cargo" && -f "$HOME/.cargo/env" ]]; then
     . "$HOME/.cargo/env"
 fi
+
+
+if command -v direnv 2>&1 >/dev/null
+then
+    eval "$(direnv hook bash)"
+fi
